fc581b285be1f5da61a48334ae28ccf7be619201
Víctor Olvera <vhocar@gmail.com>
2023-12-15 11:39:17 -0700

    Test1

--- src/views/Session.js
+++ src/views/Session.js
@@ -58,17 +58,9 @@ function Session() {
             let secondaryVideo = ''
             let secondaryVideoPath = ''
             let fileNames = {Audio:'', Video:'', Chat:''}
-            let cloudFrontDomain = process.env.REACT_APP_CLOUDFRONT_DOMAIN
             for (let i = 0; i < object.length; i++) {
                 if (object[i].name.indexOf('shared_screen_with_speaker_view') !== -1) {
-                    // Victor H. Olvera modifications start here.
-                    // const s3LinkVideo = object[i].path
-                    // const s3DomainUrl = ( new URL( s3LinkVideo ) )
-                    // const s3Domain = s3DomainUrl.hostname
-                    // const cloudFrontUrl = s3LinkVideo.replace( s3Domain, cloudFrontDomain )
-                    // setLinkVideo(cloudFrontUrl)
                     setLinkVideo(object[i].path)
-                    // Victor H. Olvera modifications end here.
                     fileNames={ ...fileNames, Video: object[i].name }
                     validateVideo = true
                 }
@@ -77,14 +69,7 @@ function Session() {
                     secondaryVideoPath =object[i].path
                 }
                 if (object[i].name.indexOf('audio_only') !== -1) {
-                    // Victor H. Olvera modifications start here.
-                    // const s3LinkAudio = object[i].path
-                    // const s3DomainUrl = ( new URL( s3LinkAudio ) )
-                    // const s3Domain = s3DomainUrl.hostname
-                    // const cloudFrontUrl = s3LinkAudio.replace( s3Domain, cloudFrontDomain )
-                    // setLinkAudio(cloudFrontUrl)
                     setLinkAudio(object[i].path)
-                    // Victor H. Olvera modifications end here.
                     fileNames={ ...fileNames, Audio: object[i].name }
                     validateAudio = true
                 }
